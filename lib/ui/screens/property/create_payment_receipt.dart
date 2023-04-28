574ade25bfca5847ff4527df9b7bd5a8b399ed38
rjcalifornia <rodolfohernan20@gmail.com>
2023-04-28 13:10:25 -0600

    Changes to be committed: 	modified:   lib/ui/screens/property/create_payment_receipt.dart

--- lib/ui/screens/property/create_payment_receipt.dart
+++ lib/ui/screens/property/create_payment_receipt.dart
@@ -18,7 +18,7 @@ class _CreateReceiptScreenState extends State<CreateReceiptScreen> {
   final PaymentsBloc _paymentsBloc = PaymentsBloc();
   late List monthsList = [];
 
-  List<PaymentType>? paymentType;
+  List<PaymentType> paymentType = [];
 
   Future _getReceiptData() async {
     final monthsJson = await _paymentsBloc.obtenerMeses();
@@ -74,32 +74,22 @@ class _CreateReceiptScreenState extends State<CreateReceiptScreen> {
                 ),
               ),
               const SizedBox(
-                height: 18,
+                height: 8,
               ),
-              StreamBuilder(
-                stream: bloc.paymentStream,
-                builder: (context, AsyncSnapshot snapshot) {
-                  return TextField(
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    onChanged: bloc.changePayment,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: const Color(0xfff6f6f6),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide.none),
-                        icon: const Icon(Icons.payment),
-                        hintText: "Ingrese una cantidad",
-                        labelText: 'Escriba la cantidad recibida',
-                        errorText: snapshot.hasError
-                            ? snapshot.error.toString()
-                            : null),
-                  );
-                },
+              Text(
+                widget.lease!.propertyId.name.toString(),
+                style: const TextStyle(
+                  color: BrandColors.rausch,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21,
+                ),
               ),
               const SizedBox(
-                height: 18,
+                height: 28,
+              ),
+              const Text(
+                "Mes a cancelar",
+                style: TextStyle(color: BrandColors.hof),
               ),
               Container(
                 decoration: BoxDecoration(
@@ -108,9 +98,10 @@ class _CreateReceiptScreenState extends State<CreateReceiptScreen> {
                 ),
                 child: DropdownButtonFormField(
                   decoration: const InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none),
+                    enabledBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
                   icon: const Icon(Icons.arrow_drop_down),
                   iconSize: 24,
                   elevation: 16,
@@ -138,6 +129,79 @@ class _CreateReceiptScreenState extends State<CreateReceiptScreen> {
                   },
                   value: selectedMonth,
                 ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "Tipo de pago",
+                style: TextStyle(color: BrandColors.hof),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color(0xfff6f6f6),
+                ),
+                child: DropdownButtonFormField(
+                  decoration: const InputDecoration(
+                      enabledBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none),
+                  icon: const Icon(Icons.arrow_drop_down),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.black),
+                  isExpanded: true,
+                  hint: const Center(child: Text("Seleccione el tipo de pago")),
+                  items: paymentType.map((item) {
+                    //print(item['id']);
+                    return DropdownMenuItem(
+                      value: item.id.toString(),
+                      child: Center(
+                        child: Text(item.name.toString()),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    //bloc.changeMonthCancelled(value.toString());
+                    bloc.changePaymentType(value.toString());
+                    setState(() {
+                      selectedMonth = value.toString();
+                      // print(selectedMonth);
+                      //print(selectedMonth);
+                      //print(bloc.getMonthCancelled);
+                    });
+                  },
+                  value: selectedMonth,
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "Cantidad",
+                style: TextStyle(color: BrandColors.hof),
+              ),
+              StreamBuilder(
+                stream: bloc.paymentStream,
+                builder: (context, AsyncSnapshot snapshot) {
+                  return TextField(
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    onChanged: bloc.changePayment,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color(0xfff6f6f6),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none),
+                        hintText: "Escriba la cantidad recibida",
+                        //labelText: 'Escriba la cantidad recibida',
+                        errorText: snapshot.hasError
+                            ? snapshot.error.toString()
+                            : null),
+                  );
+                },
               ),
             ]),
           ),
