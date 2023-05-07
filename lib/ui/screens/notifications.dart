7d79f2d8e7efe107d7b8e1578d533b356eeacf46
rjcalifornia <rodolfohernan20@gmail.com>
2023-05-07 12:13:53 -0600

    Changes to be committed: 	modified:   lib/ui/screens/notifications.dart

--- lib/ui/screens/notifications.dart
+++ lib/ui/screens/notifications.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_management_app/global.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -11,6 +12,42 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     //double width = MediaQuery.of(context).size.width;
-    return GestureDetector();
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: BrandColors.rausch,
+            elevation: 0,
+            bottom: const TabBar(
+                labelColor: Colors.redAccent,
+                unselectedLabelColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    color: Colors.white),
+                tabs: [
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Notificaciones"),
+                    ),
+                  ),
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Alertas"),
+                    ),
+                  ),
+                ]),
+          ),
+          body: TabBarView(children: [
+            Container(
+              child: Icon(Icons.apps),
+            ),
+            Icon(Icons.movie),
+          ]),
+        ));
   }
 }
