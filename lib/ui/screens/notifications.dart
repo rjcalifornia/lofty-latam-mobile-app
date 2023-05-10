a05cc046e3d7cc7c042f1ec1fc0d596ac88f1e92
rjcalifornia <rodolfohernan20@gmail.com>
2023-05-10 13:15:57 -0600

    Changes to be committed: 	modified:   lib/ui/screens/notifications.dart

--- lib/ui/screens/notifications.dart
+++ lib/ui/screens/notifications.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:home_management_app/bloc/payments_bloc.dart';
 import 'package:home_management_app/global.dart';
 import 'package:http/http.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -70,11 +71,60 @@ class _NotificationScreenState extends State<NotificationScreen> {
                 ]),
           ),
           body: TabBarView(children: [
-            Container(
-              child: Column(
-                children: [const Icon(Icons.apps)],
-              ),
-            ),
+            FutureBuilder(
+                future: loadNotifications,
+                builder: (((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                      padding: const EdgeInsets.only(
+                        top: 25,
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                              child: ListView.builder(
+                            itemCount: notifications?.length,
+                            itemBuilder: (context, i) {
+                              return Container(
+                                margin: const EdgeInsets.all(15),
+                                padding: const EdgeInsets.all(25),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(9),
+                                  color: Color(0xffe6e1ff),
+                                ),
+                                child: Row(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(9),
+                                      child: Icon(Icons.warning_outlined),
+                                    ),
+                                    SizedBox(width: 15),
+                                    Expanded(
+                                        child: Column(
+                                      children: [
+                                        Text(
+                                          notifications![i].toString(),
+                                          style: TextStyle(
+                                              color: BrandColors.foggy,
+                                              fontSize: 16),
+                                        )
+                                      ],
+                                    ))
+                                  ],
+                                ),
+                              );
+                            },
+                          ))
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Center(
+                      child: LoadingAnimationWidget.inkDrop(
+                          color: BrandColors.arches, size: 28),
+                    );
+                  }
+                }))),
             Icon(Icons.movie),
           ]),
         ));
