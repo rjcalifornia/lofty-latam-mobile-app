c61fba7f918b38f6e06263ce49b1f1c8e2e8ebc6
rjcalifornia <rodolfohernan20@gmail.com>
2023-06-07 14:16:09 -0600

    Changes to be committed: 	modified:   lib/bloc/lease_bloc.dart 	modified:   lib/ui/screens/property/create_lease.dart

--- lib/bloc/lease_bloc.dart
+++ lib/bloc/lease_bloc.dart
@@ -46,4 +46,80 @@ class LeaseBloc with Validators {
       _tenantUsernameController.sink.add;
   Function(String) get changeTenantPhone => _tenantPhoneController.sink.add;
   Function(String) get changeTenantEmail => _tenantEmailController.sink.add;
+
+  Stream<String> get rentTypeIdStream =>
+      _rentTypeIdController.stream.transform(validateLeaseFields);
+  Stream<String> get contractDateStream =>
+      _contractDateController.stream.transform(validateLeaseFields);
+  Stream<String> get paymentDateStream =>
+      _paymentDateController.stream.transform(validateLeaseFields);
+  Stream<String> get expirationDateStream =>
+      _expirationDateController.stream.transform(validateLeaseFields);
+  Stream<String> get priceStream =>
+      _priceController.stream.transform(validateLeaseFields);
+  Stream<String> get depositStream =>
+      _depositController.stream.transform(validateLeaseFields);
+  Stream<String> get tenantNameStream =>
+      _tenantNameController.stream.transform(validateLeaseFields);
+  Stream<String> get tenantLastnameStream =>
+      _tenantLastnameController.stream.transform(validateLeaseFields);
+  Stream<String> get tenantUsernameStream =>
+      _tenantLastnameController.stream.transform(validateLeaseFields);
+  Stream<String> get tenantPhoneStream =>
+      _tenantPhoneController.stream.transform(validateLeaseFields);
+  Stream<String> get tenantEmailStream =>
+      _tenantEmailController.stream.transform(validateLeaseFields);
+
+  String? get rentTypeId => _rentTypeIdController.value;
+  String? get contractDate => _contractDateController.value;
+  String? get paymentDate => _paymentDateController.value;
+  String? get expirationDate => _expirationDateController.value;
+  String? get price => _priceController.value;
+  String? get deposit => _depositController.value;
+  String? get tenantName => _tenantNameController.value;
+  String? get tenantLastname => _tenantLastnameController.value;
+  String? get tenantUsername => _tenantUsernameController.value;
+  String? get tenantPhone => _tenantPhoneController.value;
+  String? get tenantEmail => _tenantEmailController.value;
+
+  // Stream<bool> get verifyLeaseData => CombineLatestStream.combine5(
+  //     rentTypeIdStream,
+  //     contractDateStream,
+  //     paymentDateStream,
+  //     expirationDateStream,
+  //     priceStream,
+  //     (a, b, c, d, e) {});
+
+  Stream<bool> get verifyLeaseData => CombineLatestStream([
+        rentTypeIdStream,
+        contractDateStream,
+        paymentDateStream,
+        expirationDateStream,
+        priceStream,
+        depositStream,
+        tenantNameStream,
+        tenantLastnameStream,
+        tenantUsernameStream,
+        tenantPhoneStream,
+        tenantEmailStream
+      ], (_) => true);
+
+  Future getRentType() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final accessToken = prefs.getString("access_token");
+
+    var rentTypeJson = await http.get(
+        Uri.parse('${authEndpoint}api/v1/catalogs/rent-type/list'),
+        headers: {
+          "Accept": "application/json",
+          'Authorization': 'Bearer $accessToken',
+        });
+
+    final rentTypeParsed =
+        json.decode(rentTypeJson.body).cast<Map<String, dynamic>>();
+
+    return rentTypeParsed
+        .map<PaymentType>((json) => PaymentType.fromJson(json))
+        .toList();
+  }
 }
--- lib/ui/screens/property/create_lease.dart
+++ lib/ui/screens/property/create_lease.dart
@@ -35,7 +35,59 @@ class _CreateLeaseScreenState extends State<CreateLeaseScreen> {
                           style: const TextStyle(
                               color: BrandColors.hof, fontSize: 20),
                         ),
-                      )
+                      ),
+
+                      const SizedBox(
+                        height: 28,
+                      ),
+                      // const Text(
+                      //   "Mes a cancelar",
+                      //   style: TextStyle(color: BrandColors.hof),
+                      // ),
+                      // StreamBuilder(
+                      //     stream: _paymentsBloc.monthCancelledStream,
+                      //     builder: (context, AsyncSnapshot snapshot) {
+                      //       return Container(
+                      //         decoration: BoxDecoration(
+                      //           borderRadius: BorderRadius.circular(15),
+                      //           color: const Color(0xfff6f6f6),
+                      //         ),
+                      //         child: DropdownButtonFormField(
+                      //           decoration: const InputDecoration(
+                      //             enabledBorder: InputBorder.none,
+                      //             disabledBorder: InputBorder.none,
+                      //             focusedBorder: InputBorder.none,
+                      //           ),
+                      //           icon: const Icon(Icons.arrow_drop_down),
+                      //           iconSize: 24,
+                      //           elevation: 16,
+                      //           style: const TextStyle(color: Colors.black),
+                      //           isExpanded: true,
+                      //           hint: const Center(
+                      //               child: Text("Seleccione el mes a cancelar")),
+                      //           items: monthsList.map((item) {
+                      //             //print(item['id']);
+                      //             return DropdownMenuItem(
+                      //               value: item['id'].toString(),
+                      //               child: Center(
+                      //                 child: Text(item['label']),
+                      //               ),
+                      //             );
+                      //           }).toList(),
+                      //           onChanged: (value) {
+                      //             _paymentsBloc
+                      //                 .changeMonthCancelled(value.toString());
+                      //             setState(() {
+                      //               selectedMonth = value.toString();
+                      //               // print(selectedMonth);
+                      //               //print(selectedMonth);
+                      //               //print(bloc.getMonthCancelled);
+                      //             });
+                      //           },
+                      //           value: selectedMonth,
+                      //         ),
+                      //       );
+                      //     }),
                     ]))));
   }
 }
